723b9e6cb7bf89ff8a3e1d9d2bf8e2db340a35e0
Alex Green <alexkenta@gmail.com>
2018-06-15 18:50:06 -0700

    committing Group Chat

---
+++ groupChat/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path")
+// const session = require("express-session")
+const port = process.env.PORT || 8000;
+const app = express();
+const all_messages = [] //list to push all messages into
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(session({secret: 'supersecret'}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "./views"));
+
+app.get('/', function(request, response){
+    response.render('index')
+})
+
+const server = app.listen(port, function(){
+    console.log(`Listening on port ${port}`)
+});
+
+const io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+    console.log('Client/socket is connected!');
+    console.log('Client/socket id is: ' + socket.id)
+//when a new user logs on, emit an announcement to everyone. Disabled for now.
+    // socket.on("new_user_logged_on", function(data){
+    //     io.emit("new_user_announcement", {name: data.name})
+    //     console.log(data.name)
+    //})
+ //listen for new message, broadcast the message and user to everyone, push into all_messages list
+    socket.on("new_message", function(data){
+        io.emit("post_new_message", {name: data.name, message: data.message})
+        all_messages.push({name: data.name, message: data.message})
+        console.log(data.name, data.message)
+    })
+//listen for user logging in, take data(name) and EMIT all previous messages back to user.
+    socket.on("load_page", function(data){
+        socket.emit("get_all_messages", {name: data.name, messages: all_messages})
+    })
+})
